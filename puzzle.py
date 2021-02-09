49cdaaaefe901e1f144bdfefd7c37c7978d9b074
ko-mary <mariia.plys@ucu.edu.ua>
2021-02-09 23:36:54 +0200

    final version

--- puzzle.py
+++ puzzle.py
@@ -1,20 +1,20 @@
 '''
 https://github.com/ko-mary/puzzle
 '''
-import doctest
+
 def check_lines(board: list) -> bool:
     '''
     Check if there is no problem with generated board lines.
 
     >>> check_lines(["**** ****",\
-     "***1 ****",\
-      "**  3****",\
-       "* 4 1****",\
-        "     9 5 ",\
-         " 6  83  *",\
-          "3   1  **",\
-           "  8  2***",\
-            "  2  ****"])
+"***1 ****",\
+"**  3****",\
+"* 4 1****",\
+"     9 5 ",\
+" 6  83  *",\
+"3   1  **",\
+"  8  2***",\
+"  2  ****"])
     True
     '''
     for line in board:
@@ -116,5 +116,3 @@ def validate_board(board: list) -> bool:
     if check_colour(board) == False:
         return False
     return True
-
-doctest.testmod()
